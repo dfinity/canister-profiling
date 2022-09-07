abc1c926a3688a3d583b9829cdc98d2fb7df708d
Yan Chen <yan.chen@dfinity.org>
2022-09-07 12:42:59 -0700

    fix

---
+++ collections/rust/src/btreemap/src/lib.rs
@@ -0,0 +1,100 @@
+use std::collections::BTreeMap;
+use std::cell::RefCell;
+
+struct Random {
+    state: u32,
+    size: Option<u32>,
+    ind: u32,
+}
+impl Random {
+    pub fn new(size: Option<u32>, seed: u32) -> Self {
+        Random { state: seed, size, ind: 0 }
+    }
+}
+impl Iterator for Random {
+    type Item = u32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(size) = self.size {
+            self.ind += 1;
+            if self.ind > size {
+                return None;
+            }
+        }
+        self.state = self.state * 48271 % 0x7fffffff;
+        Some(self.state)
+    }
+}
+
+thread_local! {
+    static MAP: RefCell<BTreeMap<u32, String>> = RefCell::default();
+    static RAND: RefCell<Random> = RefCell::new(Random::new(None, 42));
+}
+
+#[ic_cdk_macros::update]
+fn generate(size: u32) {
+    let rand = Random::new(Some(size), 1);
+    let iter = rand.map(|x| (x, x.to_string()));
+    MAP.with(|map| {
+        let mut map = map.borrow_mut();
+        for (k,v) in iter {
+            map.insert(k,v);
+        }
+    });
+}
+
+#[ic_cdk_macros::update]
+fn get(x: u32) -> Option<String> {
+    MAP.with(|map| map.borrow().get(&x).cloned())
+}
+
+#[ic_cdk_macros::update]
+fn put(k: u32, v: String) {
+    MAP.with(|map| map.borrow_mut().insert(k, v));
+}
+
+#[ic_cdk_macros::update]
+fn remove(x: u32) {
+    MAP.with(|map| map.borrow_mut().remove(&x));
+}
+
+#[ic_cdk_macros::update]
+fn batch_get(n: u32) {
+    MAP.with(|map| {
+        let map = map.borrow();
+        RAND.with(|rand| {
+            let mut rand = rand.borrow_mut();
+            for _ in 0..n {
+                let k = rand.next().unwrap();
+                map.get(&k);
+            }
+        })
+    })
+}
+
+#[ic_cdk_macros::update]
+fn batch_put(n: u32) {
+    MAP.with(|map| {
+        let mut map = map.borrow_mut();
+        RAND.with(|rand| {
+            let mut rand = rand.borrow_mut();
+            for _ in 0..n {
+                let k = rand.next().unwrap();
+                map.insert(k, k.to_string());
+            }
+        })
+    })
+}
+
+#[ic_cdk_macros::update]
+fn batch_remove(n: u32) {
+    MAP.with(|map| {
+        let mut map = map.borrow_mut();
+        RAND.with(|rand| {
+            let mut rand = rand.borrow_mut();
+            for _ in 0..n {
+                let k = rand.next().unwrap();
+                map.remove(&k);
+            }
+        })
+    })
+}
