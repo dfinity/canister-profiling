9d7dcfa526dedd68cf15a520bcdd21a776bada36
Yan Chen <yan.chen@dfinity.org>
2022-09-10 19:40:52 -0700

    rust pub-sub

--- pub-sub/motoko/perf.sh
+++ pub-sub/motoko/perf.sh
@@ -4,12 +4,17 @@ load "../../prelude.sh";
 let pub = install(wasm_profiling(".dfx/local/canisters/pub/pub.wasm"), encode (), null);
 let sub = install(wasm_profiling(".dfx/local/canisters/sub/sub.wasm"), encode (), null);
 
+let file = "README.md";
+output(file, "\n# Publisher Subscriber\n\n| |subscribe|publish|\n|--|--:|--:|\n");
+
 call sub.init(stringify(pub), "Apples");
-flamegraph(sub, "Subscribe Apples", "subscribe.svg");
-flamegraph(pub, "Register subscriber (called by sub canister)", "pub_register.svg");
+output(file, stringify("|Motoko|[", __cost__, "](mo_subscribe.svg) [callee](mo_pub_register.svg)|"));
+flamegraph(sub, "Subscribe Apples", "mo_subscribe.svg");
+flamegraph(pub, "Register subscriber (called by sub canister)", "mo_pub_register.svg");
 
 call pub.publish(record { topic = "Apples"; value = 42 });
-flamegraph(pub, "Publish Apples", "publish.svg");
+output(file, stringify("[", __cost__, "](mo_publish.svg) [callee](mo_sub_update.svg)|\n"));
+flamegraph(pub, "Publish Apples", "mo_publish.svg");
 call sub.getCount();
 assert _ == (42 : nat);
-flamegraph(sub, "Update subscriber (callback from pub canister)", "sub_update.svg");
+flamegraph(sub, "Update subscriber (callback from pub canister)", "mo_sub_update.svg");
---
+++ pub-sub/rust/perf.sh
@@ -0,0 +1,19 @@
+#!ic-repl
+load "../../prelude.sh";
+
+let pub = install(wasm_profiling(".dfx/local/canisters/pub/pub.wasm"), encode (), null);
+let sub = install(wasm_profiling(".dfx/local/canisters/sub/sub.wasm"), encode (), null);
+
+let file = "README.md";
+
+call sub.setup_subscribe(pub, "Apples");
+output(file, stringify("|Rust|[", __cost__, "](rs_subscribe.svg) [callee](rs_pub_register.svg)|"));
+flamegraph(sub, "Subscribe Apples", "rs_subscribe.svg");
+flamegraph(pub, "Register subscriber (called by sub canister)", "rs_pub_register.svg");
+
+call pub.publish(record { topic = "Apples"; value = 42 });
+output(file, stringify("[", __cost__, "](rs_publish.svg) [callee](rs_sub_update.svg)|\n"));
+flamegraph(pub, "Publish Apples", "rs_publish.svg");
+call sub.get_count();
+assert _ == (42 : nat64);
+flamegraph(sub, "Update subscriber (callback from pub canister)", "rs_sub_update.svg");
