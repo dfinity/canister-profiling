3e5905c4e5c47770b193687ef5d82068c3fb4802
Matthew Hammer <matthew.hammer@gmail.com>
2022-09-26 17:29:16 -0600

    Include Motoko VM-based test canister. (#11)
    * Include Motoko VM-based test canister.
    
    * using Core.eval from motoko 0.0.11 now.
    
    * sketch get case.
    
    * perf.sh ic-repl script
    
    * dfx.json
    
    * Replace pattern matching with convert()
    
    * adjust dfx build movm to use binaryen opts.
    
    * try more
    
    * 20220922
    
    * 801.53 KiB
    
    * 1.2Mb with Core.step
    
    * done experimenting.
    
    * Motoko 0.0.16, and parse_static
    
    * Wasm too big.  Trying to remove num_bigint dep.
    
    * no effect from this (because it wasn't used anyway.)
    
    * features off, but no change. confused.
    
    * use a previous commit, and overcome size issue.
    
    * movm test canister is implemented.
    
    * fix var uses.
    
    * another try, with more debug output
    
    * fix
    
    * Add imrc_hashmap benchmark
    
    * Fix
    
    * another try for imrc_hashmap
    
    * stray underscore
    
    Co-authored-by: Ryan Vandersmith <ryan.vandersmith@dfinity.org>

---
+++ collections/rust/src/movm/src/lib.rs
@@ -0,0 +1,181 @@
+//use candid::Nat;
+//use fxhash::FxHashMap;
+use motoko::{ast::Literal, value::Value, vm_types::{Core, Limits}};
+use motoko_proc_macro::parse_static;
+use std::cell::RefCell;
+
+thread_local! {
+    static CORE: RefCell<Core> = RefCell::new(
+        Core::new(
+            parse_static!("var map = prim \"hashMapNew\" ()")
+                .clone()
+        )
+    );
+}
+
+fn val_from_u32(x: u32) -> Value {
+    Value::from_literal(Literal::Nat(format!("{}", x))).unwrap()
+}
+
+fn val_from_string(s: String) -> Value {
+    Value::from_literal(Literal::Text(s)).unwrap()
+}
+
+#[ic_cdk_macros::update]
+fn generate(size: u32) {
+    CORE.with(|core| {
+        (core.borrow_mut()).continue_(&Limits::none()).unwrap();
+        (core.borrow_mut())
+            .eval_open_block(
+                vec![("size", val_from_u32(size))],
+                parse_static!(
+                 "
+                 var i = prim \"fastRandIterNew\" (?size, 1);
+                 var j = {
+                   next = func () {
+                     let (n, i_) = prim \"fastRandIterNext\" i;
+                     i := i_;
+                     n
+                   }
+                 };
+                 for (x in j) {
+                   let s = prim \"natToText\" x;
+                   let (m, _) = prim \"hashMapPut\" (map, x, s);
+                   map := m;
+                 }"
+                )
+                .clone(),
+            )
+            .unwrap();
+        println!("{:?}", &(core.borrow()));
+    })
+}
+
+#[ic_cdk_macros::update]
+fn get(x: u32) -> Option<String> {
+    let _r = CORE
+        .with(|core| {
+            (core.borrow_mut()).continue_(&Limits::none()).unwrap();
+            (core.borrow_mut()).eval_open_block(
+                vec![("x", val_from_u32(x))],
+                parse_static!("prim \"hashMapGet\" (map, x)").clone(),
+            )
+        })
+        .unwrap();
+    None
+}
+
+#[ic_cdk_macros::update]
+fn put(k: u32, v: String) {
+    CORE.with(|core| {
+        (core.borrow_mut()).continue_(&Limits::none()).unwrap();
+        (core.borrow_mut()).eval_open_block(
+            vec![("k", val_from_u32(k)), ("v", val_from_string(v))],
+            parse_static!(
+                "
+                 let (m, _) = prim \"hashMapPut\" (map, k, v);
+                 map := m"
+            )
+            .clone(),
+        )
+    })
+    .unwrap();
+}
+
+#[ic_cdk_macros::update]
+fn remove(x: u32) {
+    CORE.with(|core| {
+        (core.borrow_mut()).continue_(&Limits::none()).unwrap();
+        (core.borrow_mut()).eval_open_block(
+            vec![("x", val_from_u32(x))],
+            parse_static!(
+                "
+                 let (m, _) = prim \"hashMapRemove\" (map, x);
+                 map := m"
+            )
+            .clone(),
+        )
+    })
+    .unwrap();
+}
+
+#[ic_cdk_macros::update]
+fn batch_get(n: u32) {
+    CORE.with(|core| {
+        (core.borrow_mut()).continue_(&Limits::none()).unwrap();
+        (core.borrow_mut()).eval_open_block(
+            vec![("size", val_from_u32(n))],
+            parse_static!(
+                "
+                var i = prim \"fastRandIterNew\" (?size, 1);
+                var j = {
+                  next = func () {
+                    let (n, i_) = prim \"fastRandIterNext\" i;
+                    i := i_;
+                    n
+                  }
+                 };
+                 for (x in j) {
+                   let _ = prim \"hashMapGet\" (map, x);
+                 }"
+            )
+            .clone(),
+        )
+    })
+    .unwrap();
+}
+
+#[ic_cdk_macros::update]
+fn batch_put(n: u32) {
+    CORE.with(|core| {
+        (core.borrow_mut()).continue_(&Limits::none()).unwrap();
+        (core.borrow_mut()).eval_open_block(
+            vec![("size", val_from_u32(n))],
+            parse_static!(
+                "
+                 var i = prim \"fastRandIterNew\" (?size, 1);
+                 var j = {
+                   next = func () {
+                     let (n, i_) = prim \"fastRandIterNext\" i;
+                     i := i_;
+                     n
+                   }
+                 };
+                 for (x in j) {
+                   let s = prim \"natToText\" x;
+                   let (m, _) = prim \"hashMapPut\" (map, x, s);
+                   map := m;
+                 }"
+            )
+            .clone(),
+        )
+    })
+    .unwrap();
+}
+
+#[ic_cdk_macros::update]
+fn batch_remove(n: u32) {
+    CORE.with(|core| {
+        (core.borrow_mut()).continue_(&Limits::none()).unwrap();
+        (core.borrow_mut()).eval_open_block(
+            vec![("size", val_from_u32(n))],
+            parse_static!(
+                "
+                 var i = prim \"fastRandIterNew\" (?size, 1);
+                 var j = {
+                   next = func () {
+                     let (n, i_) = prim \"fastRandIterNext\" i;
+                     i := i_;
+                     n
+                   }
+                 };
+                 for (x in j) {
+                   let (m, _) = prim \"hashMapRemove\" (map, x);
+                   map := m;
+                 }"
+            )
+            .clone(),
+        )
+    })
+    .unwrap();
+}
