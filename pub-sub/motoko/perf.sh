5cad935bea688ea5aabd17399cfa8e9b1ab021fe
Yan Chen <yan.chen@dfinity.org>
2022-11-15 20:22:01 -0800

    fix

--- pub-sub/motoko/perf.sh
+++ pub-sub/motoko/perf.sh
@@ -7,14 +7,14 @@ let sub = install(wasm_profiling(".dfx/local/canisters/sub/sub.wasm"), encode ()
 let file = "README.md";
 output(file, "\n# Publisher & Subscriber\n\n| |subscribe|publish|\n|--|--:|--:|\n");
 
-call sub.init(stringify(pub), "Apples");
+let caller = call sub.init(stringify(pub), "Apples");
 flamegraph(sub, "Subscribe Apples", "mo_subscribe.svg");
 let callee_cost = flamegraph(pub, "Register subscriber (called by sub canister)", "mo_pub_register.svg");
-output(file, stringify("|Motoko|[caller (", __cost__, ")](mo_subscribe.svg) / [callee (", callee_cost, ")](mo_pub_register.svg)|"));
+output(file, stringify("|Motoko|[caller (", __cost_caller, ")](mo_subscribe.svg) / [callee (", callee_cost, ")](mo_pub_register.svg)|"));
 
-call pub.publish(record { topic = "Apples"; value = 42 });
+let caller = call pub.publish(record { topic = "Apples"; value = 42 });
 flamegraph(pub, "Publish Apples", "mo_publish.svg");
 call sub.getCount();
 assert _ == (42 : nat);
 let callee_cost = flamegraph(sub, "Update subscriber (callback from pub canister)", "mo_sub_update.svg");
-output(file, stringify("[caller (", __cost__, ")](mo_publish.svg) / [callee (", callee_cost, ")](mo_sub_update.svg)|\n"));
+output(file, stringify("[caller (", __cost_caller, ")](mo_publish.svg) / [callee (", callee_cost, ")](mo_sub_update.svg)|\n"));
