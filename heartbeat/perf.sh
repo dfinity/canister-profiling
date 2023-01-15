6546ef3c310f0b864845b98a3ad5afd0647b0e23
Yan Chen <yan.chen@dfinity.org>
2023-01-14 17:58:30 -0800

    bump

--- heartbeat/perf.sh
+++ heartbeat/perf.sh
@@ -1,39 +1,41 @@
 #!ic-repl
 load "../prelude.sh";
 
-let motoko = wasm_profiling("motoko/.dfx/local/canisters/heartbeat/heartbeat.wasm");
+let heartbeat_mo = wasm_profiling("motoko/.dfx/local/canisters/heartbeat/heartbeat.wasm");
 let timer_mo = wasm_profiling("motoko/.dfx/local/canisters/timer/timer.wasm");
-let rust = wasm_profiling("rust/.dfx/local/canisters/heartbeat/heartbeat.wasm");
+let heartbeat_rs = wasm_profiling("rust/.dfx/local/canisters/heartbeat/heartbeat.wasm");
 let timer_rs = wasm_profiling("rust/.dfx/local/canisters/timer/timer.wasm");
 
 let file = "README.md";
 
 function heartbeat_perf(wasm, title) {
   let cid = install(wasm, encode (), null);
-  let file = stringify("heartbeat_", title);
-  let cost = flamegraph(cid, stringify(title, "_heartbeat"), file);
-  output(file, stringify("|", title, "|[", cost, "](", file, ".svg)", "|\n"));
+  let svg = stringify(title, "_heartbeat.svg");
+  let cost = flamegraph(cid, stringify(title, "_heartbeat"), svg);
+  output(file, stringify("|", title, "|[", cost, "](", svg, ")|\n"));
 };
 function timer_perf(wasm, title) {
   let cid = install(wasm, encode (), null);
   output(file, stringify("|", title, "|"));
   
-  call cid.setTimer(0);
+  let tid = call cid.setTimer(0);
+  call cid.__toggle_entry();
   // A second update call can usually capture both the job and setTimer, but it is flaky.
-  let tid = call cid.setTimer(10);
   let svg = stringify(title, "_setTimer.svg");
-  output(file, stringify("[", __cost_tid, "](", svg, ")|"));
-  flamegraph(cid, stringify(title, ".setTimer(0)"), svg);
+  let cost = flamegraph(cid, stringify(title, ".setTimer(0)"), svg);
+  output(file, stringify("[", cost, "](", svg, ")|"));
 
+  call cid.__toggle_entry();
+  let tid = call cid.setTimer(10);
   call cid.cancelTimer(tid);
   let svg = stringify(title, "_cancelTimer.svg");
-  output(file, stringify("[", __cost__, "](", svg, ")|\n"));
   flamegraph(cid, stringify(title, ".cancelTimer"), svg);
+  output(file, stringify("[", __cost__, "](", svg, ")|\n"));
 };
 
 output(file, "\n## Heartbeat\n\n| |heartbeat|\n|--:|--:|\n");
-heartbeat_perf(motoko, "Motoko");
-heartbeat_perf(rust, "Rust");
+heartbeat_perf(heartbeat_mo, "Motoko");
+heartbeat_perf(heartbeat_rs, "Rust");
 
 output(file, "\n## Timer\n\n| |setTimer|cancelTimer|\n|--:|--:|--:|\n");
 timer_perf(timer_mo, "Motoko");
