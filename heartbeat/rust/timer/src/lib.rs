416ad1a6c6a50ef72d4f55827f1c577afe340b5c
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2023-02-04 21:10:36 -0800

    bump candid to 0.9 (#26)
    * bump candid to 0.9
    
    * fix timer

--- heartbeat/rust/timer/src/lib.rs
+++ heartbeat/rust/timer/src/lib.rs
@@ -1,16 +1,17 @@
 #![allow(non_snake_case)]
 
-use ic_cdk::{timer, update};
+use ic_cdk::update;
 use std::cell::RefCell;
 use std::time::Duration;
+use ic_cdk_timers::{set_timer, clear_timer, TimerId};
 
 thread_local! {
-    static ID: RefCell<timer::TimerId> = RefCell::default();
+    static ID: RefCell<TimerId> = RefCell::default();
 }
 
 #[update]
 fn setTimer(sec: u64) {
-    let tid = timer::set_timer(Duration::from_secs(sec), || {});
+    let tid = set_timer(Duration::from_secs(sec), || {});
     ID.with(|id| {
         *id.borrow_mut() = tid;
     });
@@ -19,7 +20,7 @@ fn setTimer(sec: u64) {
 #[update]
 fn cancelTimer() {
     ID.with(|id| {
-        timer::clear_timer(*id.borrow());
+        clear_timer(*id.borrow());
     });
 }
 
