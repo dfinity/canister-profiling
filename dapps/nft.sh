8e14dd592a46f8b69e78ed708489a76622df62d9
Yan Chen <yan.chen@dfinity.org>
2023-02-07 10:40:10 -0800

    fix

--- dapps/nft.sh
+++ dapps/nft.sh
@@ -11,50 +11,47 @@ let motoko = wasm_profiling("motoko/.dfx/local/canisters/dip721_nft/dip721_nft.w
 let rust = wasm_profiling("rust/.dfx/local/canisters/dip721_nft/dip721_nft.wasm");
 
 let file = "README.md";
-output(file, "\n| |binary_size|init|mint_token|transfer_token|\n|--|--:|--:|--:|--:|\n");
+output(file, "\n## DIP721-NFT\n\n| |binary_size|init|mint_token|transfer_token|\n|--|--:|--:|--:|--:|\n");
 
 function perf(wasm, title) {
-let init = encode fake.__init_args(
-  record {
-    logo = record {
-      logo_type = "image/png";
-      data = "";
-    };
-    name = "My DIP721";
-    symbol = "DFXB";
-    maxLimit = 10;
-  }
-);
-
-let NFT = install(wasm, init, null);
-call NFT.__get_cycles();
-output(file, stringify("|", title, "|", wasm.size(), "|", _, "|"));
-
-// mint
-let _ = call NFT.mintDip721(
-  bob,
-  vec {
+  let init = encode fake.__init_args(
     record {
-      purpose = variant { Rendered };
-      data = blob "hello";
-      key_val_data = vec {
-        record { "description"; variant { TextContent = "The NFT metadata can hold arbitrary metadata" } };
-        record { "tag"; variant { TextContent = "anime" } };
-        record { "contentType"; variant { TextContent = "text/plain" } };
-        record { "locationType"; variant { Nat8Content = 4 } };
+      logo = record {
+        logo_type = "image/png";
+        data = "";
+      };
+      name = "My DIP721";
+      symbol = "DFXB";
+      maxLimit = 10;
+    }
+  );
+  let NFT = install(wasm, init, null);
+  call NFT.__get_cycles();
+  output(file, stringify("|", title, "|", wasm.size(), "|", _, "|"));
+  // mint
+  let _ = call NFT.mintDip721(
+    bob,
+    vec {
+      record {
+        purpose = variant { Rendered };
+        data = blob "hello";
+        key_val_data = vec {
+          record { "description"; variant { TextContent = "The NFT metadata can hold arbitrary metadata" } };
+          record { "tag"; variant { TextContent = "anime" } };
+          record { "contentType"; variant { TextContent = "text/plain" } };
+          record { "locationType"; variant { Nat8Content = 4 } };
+        }
       }
     }
-  }                             
-);
-let svg = stringify(title, "_nft_mint.svg");
-output(file, stringify("[", __cost__, "](", svg, ")|"));
-flamegraph(NFT, "NFT.mintDip721", "nft_mint");
-
-// transfer tokens
-let _ = call NFT.transferFromDip721(bob, alice, 0);
-let svg = stringify(title, "_nft_transfer.svg");
-output(file, stringify("[", __cost__, "](", svg, ")|\n"));
-flamegraph(NFT, "NFT.transferFromDip721", "nft_transfer");
+  );
+  let svg = stringify(title, "_nft_mint.svg");
+  output(file, stringify("[", __cost__, "](", svg, ")|"));
+  flamegraph(NFT, "NFT.mintDip721", svg);
+  // transfer tokens
+  let _ = call NFT.transferFromDip721(bob, alice, 0);
+  let svg = stringify(title, "_nft_transfer.svg");
+  output(file, stringify("[", __cost__, "](", svg, ")|\n"));
+  flamegraph(NFT, "NFT.transferFromDip721", svg);
 };
 
 perf(motoko, "Motoko");
