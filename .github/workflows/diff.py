adca888604c7efdd1e586c8c1466cbf52469fa47
Yan Chen <yan.chen@dfinity.org>
2023-03-15 17:46:56 -0700

    try

--- .github/workflows/diff.py
+++ .github/workflows/diff.py
@@ -3,18 +3,26 @@ import pandas as pd
 import markdown
 
 if len(sys.argv) < 3:
-    print("Usage: python extract_tables.py [current.md] [main.md]")
+    print("Usage: python diff.py [current.md] [main.md]")
     sys.exit(1)
 
 def read_tables(file):
-    with open(file, "r") as f:
-        md = f.read()
-        html = markdown.markdown(md, extensions=['tables'])
-        tables = pd.read_html(html, thousands = '_', index_col = 0)
-        return tables
+    try:
+        with open(file, "r") as f:
+            md = f.read()
+            html = markdown.markdown(md, extensions=['tables'])
+            tables = pd.read_html(html, thousands = '_', index_col = 0)
+            return tables
+    except OSError as e:
+        print(f"> **Warning**\n> Skip {file}. File not found.\n")
+        sys.exit(0)
 
 current = read_tables(sys.argv[1])
 main = read_tables(sys.argv[2])
+
+if len(current) != len(main):
+    print(f"> **Warning**\n> Skip {sys.argv[1]}, due to the number of tables mismatches from main branch.\n")
+    sys.exit(0)
 
 for i, (current, main) in enumerate(zip(current, main)):
     if current.shape == main.shape and all(current.columns == main.columns) and all(current.index == main.index):
@@ -25,11 +33,11 @@ for i, (current, main) in enumerate(zip(current, main)):
                 x = row[col]
                 d = diff.loc[idx, col]
                 if d != 0:
-                    result.loc[idx, col] = f"{x:_}({d:.2f}%)"
+                    result.loc[idx, col] = f"{x:_} ({d:.2f}%)"
                 else:
                     result.loc[idx, col] = f"{x:_}"
         print(result.to_markdown())
+        print(f"\n")
     else:
-        print(f"print only current")
-        print(current.to_markdown())
+        print(f"> **Warning**\n> Skip table {i} from {sys.argv[1]}, due to table shape mismatches from main branch.\n")
 
