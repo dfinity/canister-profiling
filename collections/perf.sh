e1254de3df642f3c991fb363d79c4a6495a8018f
Kento Sugama <kento.sugama@dfinity.org>
2023-05-31 10:19:29 -0700

    reenable splay

--- collections/perf.sh
+++ collections/perf.sh
@@ -4,7 +4,7 @@ load "../prelude.sh";
 let hashmap = wasm_profiling("motoko/.dfx/local/canisters/hashmap/hashmap.wasm");
 let triemap = wasm_profiling("motoko/.dfx/local/canisters/triemap/triemap.wasm");
 let rbtree = wasm_profiling("motoko/.dfx/local/canisters/rbtree/rbtree.wasm");
-// let splay = wasm_profiling("motoko/.dfx/local/canisters/splay/splay.wasm");
+let splay = wasm_profiling("motoko/.dfx/local/canisters/splay/splay.wasm");
 let btree = wasm_profiling("motoko/.dfx/local/canisters/btreemap/btreemap.wasm");
 let zhenya = wasm_profiling("motoko/.dfx/local/canisters/zhenya_hashmap/zhenya_hashmap.wasm");
 let heap = wasm_profiling("motoko/.dfx/local/canisters/heap/heap.wasm");
@@ -20,26 +20,26 @@ output(file, "\n## Map\n\n| |binary_size|generate 50k|max mem|batch_get 50|batch
 
 function perf(wasm, title, init) {
   let cid = install(wasm, encode (), null);
-  
+
   output(file, stringify("|", title, "|", wasm.size(), "|"));
   call cid.__toggle_tracing();
   call cid.generate(init);
   output(file, stringify(__cost__, "|"));
   call cid.get_mem();
   output(file, stringify(_[2], "|"));
-  
+
   call cid.__toggle_tracing();
   call cid.batch_get(50);
   let svg = stringify(title, "_get.svg");
   output(file, stringify("[", __cost__, "](", svg, ")|"));
   flamegraph(cid, stringify(title, ".batch_get"), svg);
-  
+
   call cid.batch_put(50);
   let svg = stringify(title, "_put.svg");
   output(file, stringify("[", __cost__, "](", svg, ")|"));
   flamegraph(cid, stringify(title, ".batch_put"), svg);
   call cid.get_mem();
-  
+
   call cid.batch_remove(50);
   let svg = stringify(title, "_remove.svg");
   output(file, stringify("[", __cost__, "](",svg, ")|\n"));
@@ -50,7 +50,7 @@ let init_size = 50000;
 perf(hashmap, "hashmap", init_size);
 perf(triemap, "triemap", init_size);
 perf(rbtree, "rbtree", init_size);
-// perf(splay, "splay", init_size);
+perf(splay, "splay", init_size);
 perf(btree, "btree", init_size);
 perf(zhenya, "zhenya_hashmap", init_size);
 perf(btreemap_rs, "btreemap_rs", init_size);
