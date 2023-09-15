cd0eb461728bfe0ed6107b9a64d038b41da9f76e
Claudio Russo <claudio@dfinity.org>
2023-09-16 00:38:49 +0100

    wasm-opt 03

--- utils.mk
+++ utils.mk
@@ -1,6 +1,6 @@
 define ic-wasm
-	echo "Aggressive optimize with ic-wasm"; \
-	for f in $(1)/*/*.wasm; do ic-wasm -o $$f $$f optimize O4 --inline-functions-with-loops --always-inline-max-function-size 100 --keep-name-section; done
+	echo "optimize with ic-wasm O3"; \
+	for f in $(1)/*/*.wasm; do ic-wasm -o $$f $$f optimize O3 --keep-name-section; done
 endef
 
 define build_with_mops
